d1f4746bcf7ede385fcb35a0e98447df00f8de73
Muhammad Samudra <fresh.samudra@gmail.com>
2025-08-21 14:30:18 +0700

    comment ig

--- application/models/Destana_ancaman_model.php
+++ application/models/Destana_ancaman_model.php
@@ -20,7 +20,7 @@ class Destana_ancaman_model extends CI_Model
         return $this->db->delete($this->table, ['id_destana' => $id_destana]);
     }
 
-    public function get_by_destana($id_destana)
+    public function get_by_destana($id_destana) //did i end up using this?
     {
         $this->db->select('ma.nama_ancaman');
         $this->db->from($this->table . ' da');
